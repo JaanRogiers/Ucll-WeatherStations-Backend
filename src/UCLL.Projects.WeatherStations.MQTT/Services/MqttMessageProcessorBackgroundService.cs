1cb09cbf561749bf99361b527368797c2ece2b4f
JaanRogiers <jaan.rogiers@hotmail.com>
2024-10-24 17:10:27 +0200

    MqttMessageProcessorBackgroundService

--- src/UCLL.Projects.WeatherStations.MQTT/Services/MqttMessageProcessorBackgroundService.cs
+++ src/UCLL.Projects.WeatherStations.MQTT/Services/MqttMessageProcessorBackgroundService.cs
@@ -1,20 +1,50 @@
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using UCLL.Projects.WeatherStations.MQTT.Interfaces.Services;
 using UCLL.Projects.WeatherStations.MQTT.Models;
 
 namespace UCLL.Projects.WeatherStations.MQTT.Services;
 
-public class MqttMessageProcessorBackgroundService : BackgroundService
+public class MqttMessageProcessorBackgroundService(ILogger<MqttMessageProcessorBackgroundService> logger, IMqttMessageQueue mqttMessageMqttMessageQueue) : BackgroundService
 {
+    private readonly ILogger<MqttMessageProcessorBackgroundService> _logger = logger;
+    private readonly IMqttMessageQueue _mqttMessageQueue = mqttMessageMqttMessageQueue;
+
     protected override async Task ExecuteAsync(CancellationToken cancellationToken)
     {
-        while (!cancellationToken.IsCancellationRequested)
+        _logger.LogInformation("MqttMessageProcessorBackgroundService is starting.");
+
+        try
         {
-            // Background task logic
-            await Task.Delay(1000, cancellationToken);
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                // Wait until there is data in the queue or cancellation is requested
+                if (!await _mqttMessageQueue.WaitToDequeueAsync(cancellationToken)) continue;
+
+                // Process messages as long as there are messages in the queue
+                while (!cancellationToken.IsCancellationRequested && _mqttMessageQueue.Count > 0)
+                {
+                    MqttMessage message = await _mqttMessageQueue.DequeueAsync(cancellationToken);
+
+                    await ProcessMqttMessageAsync(message, cancellationToken);
+                }
+            }
+        }
+        catch (TaskCanceledException)
+        {
+            _logger.LogInformation("Processing canceled.");
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError("Error occurred while processing MQTT messages: {ex.Message}", ex.Message);
+        }
+        finally
+        {
+            _logger.LogInformation("MqttMessageProcessorBackgroundService is stopping.");
         }
     }
 
-    private async Task ProcessMqttMessageAsync(MqttMessage mqttMessage)
+    private async Task ProcessMqttMessageAsync(MqttMessage mqttMessage, CancellationToken cancellationToken)
     {
         // Process the message
     }
