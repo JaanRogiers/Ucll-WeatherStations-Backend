7f0fb0e9d7d0eceaf013cda8319d02433086fe1d
Rogiers Jaan <75755779+JaanRogiers@users.noreply.github.com>
2024-11-08 23:32:31 +0100

    Update Program.cs

--- src/UCLL.Projects.WeatherStations.WebApi/Program.cs
+++ src/UCLL.Projects.WeatherStations.WebApi/Program.cs
@@ -12,6 +12,9 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 // add automapper for the Dto's
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
+
+
+
 // Add services to the container.
 builder.Services.AddScoped<IMeasurementRepository, MeasurementRepository>();
 builder.Services.AddScoped<IStationRepository, StationRepository>();
